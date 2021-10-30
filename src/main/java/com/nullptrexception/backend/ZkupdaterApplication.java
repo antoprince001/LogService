f65e39b0c6f579fb99810f25fe2802cd02cd135b
antoprince001 <antoprince001@gmail.com>
2021-10-30 15:33:32 +0530

    Component scan annotation import added

--- src/main/java/com/nullptrexception/backend/ZkupdaterApplication.java
+++ src/main/java/com/nullptrexception/backend/ZkupdaterApplication.java
@@ -3,12 +3,14 @@ package com.nullptrexception.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.nullptrexception.backend.repositories")
 @EntityScan(basePackages = "com.nullptrexception.backend.entity")
 @ComponentScan({ "com.nullptrexception.backend", "controller" })
+
 public class ZkupdaterApplication {
 
 	public static void main(String[] args) {
