d9cf4c0d3271699cd1aa8395ef44d9b83d5945d6
antoprince001 <antoprince001@gmail.com>
2021-10-30 16:56:01 +0530

    Security update 2

--- src/main/java/com/nullptrexception/backend/ZkupdaterApplication.java
+++ src/main/java/com/nullptrexception/backend/ZkupdaterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplicationexclude = { SecurityConfig.class }
 @EnableJpaRepositories(basePackages = "com.nullptrexception.backend.repositories")
 @EntityScan(basePackages = "com.nullptrexception.backend.entity")
 @ComponentScan(basePackages = {"com.nullptrexception.backend.Controller"})
